817abd8188ea9643409652ad485e20869516b0a6
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-04-06 19:44:08 -0400

    added fun with giphs with  urls

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require_relative "../lib/api_giphs.rb"
+
+require 'open-uri'
+
 
 welcome
 word = get_query_from_user
 get_cn_api(word)
+keyword="chuck+norris"
+url = get_giph_api(keyword)
+system("open -a Safari #{url}")
